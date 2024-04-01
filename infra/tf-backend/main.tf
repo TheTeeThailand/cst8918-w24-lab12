fe1e7f26b5cfdec5c1db9b5d288aa3cc17566544
vethusonamitAlgonquin <amit0004algonquinlive.com>
2024-04-01 16:07:58 -0400

    add outputs and create a container

--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -34,11 +34,36 @@ resource "azurerm_resource_group" "rg" {
   location = var.region
 }
 
-resource "azurerm_storage_account" "storageacconut" {
+resource "azurerm_storage_account" "githubactions" {
   name                     = "${var.labelPrefix}githubactions"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.githubactions.name
+  container_access_type = "private"
+}
+
+
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.githubactions.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.tfstate.name
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.githubactions.primary_access_key
+  sensitive = true
 }
