801f6e68e64122e1b28c17311b4aa6c1e5a6b739
vethusonamitAlgonquin <amit0004algonquinlive.com>
2024-04-01 20:26:22 -0400

    change terraform.tf to include thep's githubactions

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -11,7 +11,7 @@ terraform {
   }
 
   backend "azurerm" {
-    storage_account_name = "amit0004githubactions"
+    storage_account_name = "rung1018githubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
     use_oidc             = true
